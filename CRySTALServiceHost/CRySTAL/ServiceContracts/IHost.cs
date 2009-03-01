c796aae54577942e3442530b7f09373326fe90aa
unknown <ethiele@ethiele.com>
2009-03-01 00:41:27 -0500

    added contracts

---
+++ CRySTALServiceHost/CRySTAL/ServiceContracts/IHost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace CRySTAL.ServiceContracts
+{
+    [ServiceContract]
+    interface IHost
+    {
+        [OperationContract]
+        Dictionary<TableTypes, List<Int32>> GetTables(string sessionID);
+        [OperationContract]
+        Dictionary<TableTypes, List<Int32>> GetFreeTablesAt(string sessionID, DateTime time);
+        
+
+    }
+}
---
+++ CRySTALServiceHost/CRySTAL/ServiceContracts/IWaiter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace CRySTAL
+{
+    [ServiceContract]
+    public interface IWaiter
+    {
+        [OperationContract]
+        void PlaceOrder(string sessionID, FoodOrder order);
+        [OperationContract]
+        List<RejectedOrder> GetReturnedOrders(string sessionID);
+    }
+}
